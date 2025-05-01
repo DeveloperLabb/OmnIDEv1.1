d9f58d54f00fcd36d3cb39cecd69aab160e63d1f
Ediz Arkın Kobak <119970256+EdizArkin@users.noreply.github.com>
2025-05-01 17:32:03 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App'; // Updated path to match component location
 
 import App from './Components/App';
 import Navbar from './Components/Navbar';
