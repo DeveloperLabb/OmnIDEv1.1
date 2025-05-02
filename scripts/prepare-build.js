fffcc101acbac54d4affcef781a84e65e684faae
Heliacaa <121296219+Heliacaa@users.noreply.github.com>
2025-05-02 16:58:39 +0300

    Scripts deleted

--- scripts/prepare-build.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra');
-const path = require('path');
-
-// Clean up previous build artifacts
-console.log('Cleaning up previous build artifacts...');
-
-// We don't need to clean python_api anymore since we're using the PyInstaller output directly
-
-console.log('Build preparation completed');
